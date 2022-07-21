66623d1c741e8404d671650dd6b4aacbc958bb49
Patrik Plihal <patrik.plihal@digiconcept.net>
2022-07-21 15:18:47 +0200

    add ALT_T(ESC)

--- keyboards/crkbd/keymaps/birdspider/keymap.c
+++ keyboards/crkbd/keymaps/birdspider/keymap.c
@@ -33,12 +33,13 @@ enum custom_layers {
 
 #define KC_ALENT ALT_T(KC_ENT)
 #define KC_ARBSP ALGR_T(KC_BSPC)
+#define KC_AESC ALT_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QUERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_GRV,
+      KC_AESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_STAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RSFT_T(KC_MINS),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
