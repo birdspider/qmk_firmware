7f68360fd26b016ab02b18d08367c2a94edfb77a
Patrik Plihal <patrik.plihal@digiconcept.net>
2022-07-21 15:12:40 +0200

    add key_override for shift-<paren>

--- keyboards/crkbd/keymaps/birdspider/keymap.c
+++ keyboards/crkbd/keymaps/birdspider/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,   KC_LT,                        KC_GT, KC_RBRC, KC_PLUS, XXXXXXX, KC_HOME, KC_TILD,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,   KC_LT,                      XXXXXXX, XXXXXXX, KC_PLUS, XXXXXXX, KC_HOME, KC_TILD,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_PGUP,                      KC_PGDN, KC_RPRN,  KC_EQL, XXXXXXX,  KC_END, KC_UNDS,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_PGUP,                      KC_PGDN, XXXXXXX,  KC_EQL, XXXXXXX,  KC_END, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR,  KC_INS,                       KC_DEL, KC_RCBR, KC_MINS, XXXXXXX, KC_BSLS,  KC_DQT,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR,  KC_INS,                       KC_DEL, XXXXXXX, KC_MINS, XXXXXXX, KC_BSLS,  KC_DQT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, XXXXXXX,  KC_SPC,     KC_ENT,   RAISE, KC_ARBSP
                                       //`--------------------------'  `--------------------------'
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TILD,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_UNDS,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,  KC_DQT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -88,6 +88,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+// shift+<open> => <close>
+const key_override_t shift_LPRN_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_RPRN);
+const key_override_t shift_LBRC_override = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, KC_RBRC);
+const key_override_t shift_LCBR_override = ko_make_basic(MOD_MASK_SHIFT, KC_LCBR, KC_RCBR);
+const key_override_t shift_LT_override   = ko_make_basic(MOD_MASK_SHIFT, KC_LT, KC_GT);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &shift_LPRN_override,
+    &shift_LBRC_override,
+    &shift_LCBR_override,
+    &shift_LT_override,
+    NULL // Null terminate the array of overrides!
+};
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
