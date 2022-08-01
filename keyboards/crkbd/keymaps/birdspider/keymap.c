0e1e1169b391f851324ad875e638900b7c6a89b4
Patrik Plihal <patrik.plihal@gmail.com>
2022-08-01 21:49:54 +0200

    un-combo shift/tab

--- keyboards/crkbd/keymaps/birdspider/keymap.c
+++ keyboards/crkbd/keymaps/birdspider/keymap.c
@@ -29,8 +29,6 @@ enum custom_layers {
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
-#define KC_STAB LSFT_T(KC_TAB)
-#define KC_ARBS ALGR_T(KC_BSPC)
 #define KC_AESC ALT_T(KC_ESC)
 #define KC_GUIT LGUI_T(KC_TAB)
 
@@ -48,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_AESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_STAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RSFT_T(KC_MINS),
+      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RSFT_T(KC_MINS),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, RCTL_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -65,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, XXXXXXX, XXXXXXX,   KC_LT, KC_LBRC, KC_MINS,                      KC_PLUS, KC_RBRC,   KC_GT, XXXXXXX, KC_BSLS, RCTL_T(KC_DQT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, XXXXXXX, _______,    _______, _______, _______
-                                      //`--------------------------'  `--------------------------'  
+                                      //`--------------------------'  `--------------------------'
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3(
